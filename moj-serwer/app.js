5fd77ec1c88680b87c7a61f49273a47e465d1b69
Sebastian Musial <dev.sebas.musial@gmail.com>
2025-05-27 09:50:11 +0200

    Delete Button "Delete" for delete client

--- moj-serwer/app.js
+++ moj-serwer/app.js
@@ -25,7 +25,7 @@ app.get('/api/klienci', (req, res) => {
 app.post('/api/klienci', (req, res) => {    //nasłuchiwanie zadan typu POST(Dodanie danych), Req - dane zadania z formularzy w HTML, Res wysłanie odpowiedzi z serwera do klienta
   const { Imie, Nazwisko, Adres, Numer_Telefonu } = req.body;   //Destukturyzacja req.body który zawiera dane z frontendu z fetch - dane z formularzy input
 
-  const sql = 'INSERT INTO Client_Table (Imie, Nazwisko, Adres, Numer_Telefonu) VALUES (?, ?, ?, ?)';   //Dodaje dane w SQL. Placeholdery czyli ? zabezpieczają dane przez SQL injection
+  const sql = 'INSERT INTO Client_Table (Imie, Nazwisko, Adres, Numer_Telefonu) VALUES (?, ?, ?, ?)';   //Dodaje dane w SQL. Placeholdery czyli "?"" zabezpieczają dane przez SQL injection
   db.query(sql, [Imie, Nazwisko, Adres, Numer_Telefonu], (err, result) => {
     if (err) {
       console.error('Błąd zapisu do bazy:', err);
@@ -33,7 +33,7 @@ app.post('/api/klienci', (req, res) => {    //nasłuchiwanie zadan typu POST(Dod
     }
 
     const nowyKlient = {
-      ID_Client: result.insertId,
+      ID_Client: result.insertId, //Przypisanie wartosci ID ktora wygenerowala baza MySQL
       Imie,
       Nazwisko,
       Adres,
@@ -41,6 +41,29 @@ app.post('/api/klienci', (req, res) => {    //nasłuchiwanie zadan typu POST(Dod
     };
 
     res.status(201).json(nowyKlient);   //status(201) kod HTTP oznaczający utworzenie zasobu. Zwraca "Nowyklient" w formacie JSON do frontendu w celu dodania nowego wiersza w tabeli
+  });
+});
+
+// Usuwanie klienta po ID
+app.delete('/api/klienci/:id', (req, res) => {  //parseInt --> Konwersja na liczbe calkowita (dziesiętną - 10) poniewaz ID jest przesylane w postaci stringu
+  const id = parseInt(req.params.id, 10); //req.params.id --> Przechwycenie podanego :ID na froncie przez usera w celu udostepnienia zmiennej
+
+  if (isNaN(id)) {  //Weryfikuje poprawnosc konwersji na liczbe
+    return res.status(400).json({ error: 'Nieprawidłowe ID klienta' });
+  }
+
+  const sql = 'DELETE FROM Client_Table WHERE ID_Client = ?';
+  db.query(sql, [id], (err, result) => {
+    if (err) {
+      console.error('❌ Błąd zapytania DELETE:', err);
+      return res.status(500).json({ error: 'Błąd bazy danych' }); //500 Internal Server Error
+    }
+
+    if (result.affectedRows === 0) {  //Sprawdza czy zostal usuniety jakikolwiek rekord (result.affectedRows [liczba usuniętych rekordów] === 0 )
+      return res.status(404).json({ error: 'Nie znaleziono klienta o podanym ID' });  //404 Not Found
+    }
+
+    res.status(200).json({ message: 'Klient usunięty' });
   });
 });
 
